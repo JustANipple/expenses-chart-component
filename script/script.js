d40000127e884c857e95e43316fbdb44a80438c4
JustANipple <prandisamuel99@gmail.com>
2023-04-27 16:23:33 +0200

    modified:   script/script.js 	modified:   styles/css/main.css 	modified:   styles/css/main.css.map 	modified:   styles/scss/components/_summary.scss

--- script/script.js
+++ script/script.js
@@ -1,33 +1,61 @@
 //this gives an array of objects [{day, amount}]
 async function amountInDays() {
-    
     const request = new Request("script/data.json");
     const response = await fetch(request);
     const amounts = await response.json();
-
+    createBars(amounts);
 }
 
-//max height 9.5rem -> 100% of height
-//we manage 19 parts of 0.5rem
-//maxNumber : 9.5 = number : ?
-
-function createBars() {
+function createBars(amounts) {
+    //save a reference for the chart container
     const chart = document.querySelector(".chart_bars");
+    
     for(let i = 0; i < 7; i++) {
-        const block = document.createElement("div");
-        block.setAttribute("class", "barAndDay");
-        chart.appendChild(block);
+        //container of bar and day
+        const barDayCont = document.createElement("div");
+        barDayCont.setAttribute("class", "bar_day_container");
+        chart.appendChild(barDayCont);
+        //bar
+        const bar = document.createElement("div");
+        bar.setAttribute("class", "container_bar");
+        barDayCont.appendChild(bar);
+        //day
+        const day = document.createElement("p");
+        day.setAttribute("class", "container_day");
+        barDayCont.appendChild(day);
+        fillDays(amounts);
     }
-    const barsArr = document.querySelectorAll(".barAndDay");
-    barsArr[0].style.height = "3.16rem";
-    barsArr[1].style.height = "6.33rem";
-    barsArr[2].style.height = "9.5rem";
-    barsArr[3].style.height = "5.63rem";
-    barsArr[4].style.height = "4.24rem";
-    barsArr[5].style.height = "7.85rem";
-    barsArr[6].style.height = "4.62rem";
+    //this is the max value of the bars height in rem
+    const maxHeight = 9.5;
+    //check the highest number in the data for the proportion
+    const highest = highNumber(amounts);
+
+    const barsArr = document.querySelectorAll(".container_bar");
+    //proportion --> highest : 9.5rem = number : ? --> 9.5 * number / highest
+    for(let i = 0; i < barsArr.length; i++) {
+        barsArr[i].style.height = maxHeight * amounts[i].amount / highest + "rem";
+    }
+    
+    barsArr[new Date().getDay() - 1].setAttribute("id", "current_day");
 }
 
-createBars();
+function fillDays(amounts) {
+
+    const days = document.querySelectorAll(".container_day");
+    for(let i = 0; i < days.length; i++) {
+        days[i].textContent = amounts[i].day;
+    }
+}
+
+function highNumber(amounts) {
+
+    let highest = 0;
+    for(const data of amounts) {
+        if(data.amount > highest) {
+            highest = data.amount;
+        }
+    }
+    return highest;
+}
 
 amountInDays();
